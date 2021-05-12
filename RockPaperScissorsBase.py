549b46f983e89e0463ef6c0f24e5690000888bcc
Bhaskar Dhara <dharabhaskar@gmail.com>
2021-05-12 19:18:37 +0530

    Corrected the mistake.

--- RockPaperScissorsBase.py
+++ RockPaperScissorsBase.py
@@ -11,9 +11,9 @@ while True:
     r = random.choice(l)
     if r == i:
         print(f"computer got {r} and you chose {i} its a draw")
-    if r == "s" and i == "r" or r == "r" and i == "p":
+    if r == "s" and i == "r" or r == "r" and i == "p" or r == "p" and i == "s":
         print(f"computer got {r} and you chose {i} you win")
         break
-    if r == "r" and i == "s" or r == "p" and i == "r":
+    if r == "r" and i == "s" or r == "p" and i == "r" or r == "s" and i == "p":
         print(f"computer got {r} and you chose {i} computer wins")
         break
