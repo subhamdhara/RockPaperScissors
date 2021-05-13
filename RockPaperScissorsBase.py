3d40fa9110725d2e6ef5ce6fd3e55d5de912d86c
Bhaskar Dhara <dharabhaskar@gmail.com>
2021-05-13 18:06:02 +0530

    I changed the break of it. You need to press q for quit

--- RockPaperScissorsBase.py
+++ RockPaperScissorsBase.py
@@ -2,7 +2,7 @@ import random
 
 while True:
     try:
-        i = input("Put s for scissors, r for rock and p for paper: ")
+        i = input("Put s for scissors, r for rock, p for paper and q for quit: ")
 
     except:
         break
@@ -13,7 +13,7 @@ while True:
         print(f"computer got {r} and you chose {i} its a draw")
     if r == "s" and i == "r" or r == "r" and i == "p" or r == "p" and i == "s":
         print(f"computer got {r} and you chose {i} you win")
-        break
     if r == "r" and i == "s" or r == "p" and i == "r" or r == "s" and i == "p":
         print(f"computer got {r} and you chose {i} computer wins")
+    if i == "q":
         break
